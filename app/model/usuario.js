b16c42a23d29deb0a17fcabf21d673aee18af608
Paulo Raimundo <pauloraimundo@paulo.raimundo>
2024-06-11 18:48:34 -0300

    Criando o modelo de usuário no sequelize.

---
+++ app/model/usuario.js
@@ -0,0 +1,29 @@
+const Sequelize = require('sequelize');
+const database = require('../db');
+
+const Usuario = database.define('usuario', {
+    id: {
+        type: Sequelize.INTEGER,
+        autoIncrement: true,
+        allowNull: false,
+        primaryKey: true
+    },
+    nome: {
+        type: Sequelize.STRING,
+        allowNull: false
+    },
+    email: {
+        type: Sequelize.STRING,
+        allowNull: false,
+        unique: true
+    },
+    password: {
+        type: Sequelize.STRING,
+        allowNull: false
+    },
+    created_at: {
+        type: Sequelize.DATE,
+    }
+});
+
+module.exports = Usuario;
